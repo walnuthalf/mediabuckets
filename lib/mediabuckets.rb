5cccc0ef02a88df5038ddbae0f25187be725e229
mk <mk@example.com>
2017-12-27 22:10:03 +1100

    added RDoc annotations to Mediabuckets.arrange

--- lib/mediabuckets.rb
+++ lib/mediabuckets.rb
@@ -4,6 +4,13 @@ require "mediabuckets/trimmer"
 require "mediabuckets/action"
 
 module Mediabuckets
+  # Sorts all files from source directory by media type, 
+  # filters identical files,
+  # links, saves, or moves files to the destination directory.
+  # Params:
+  # +source+:: source directory. String. 
+  # +dest+:: destination directory. String. 
+  # +command+:: which UNIX command to use. "link", "copy", "move"  
   def self.arrange(source, dest, command)
     sourcename = Pathname.new source
     destname = Pathname.new dest
@@ -11,7 +18,7 @@ module Mediabuckets
       FileUtils.mkdir destname
     end
     if not destname.empty?
-      raise "destination directory is not empty"
+      raise "destination directory #{dest} is not empty"
     end
     logpathname = destname.join "__log__"
     logger = Logger.new(logpathname)
